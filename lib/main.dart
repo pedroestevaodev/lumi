89c4e15fc3d065aa399379318f8d38e32263b012
Caio Caminitti de Moraes <caiomoraes858@gmail.com>
2025-10-22 22:45:54 -0300

    Revert "Config setup for firebase"
    This reverts commit a3e0b47ac98cc477f71e8d9c4a3d69db9b4b6071.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart' as firebase;
-import 'firebase_options.dart';
 
-void main() async { 
-  WidgetsFlutterBinding.ensureInitialized();
-  
-  await firebase.Firebase.initializeApp( 
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  
+void main() {
   runApp(const MyApp());
 }
 
